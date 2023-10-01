9148d3192be26f26a5155eebb7944a08cd388e47
TonyJiashiChen <tonychen9661@gmail.com>
2023-10-01 11:57:37 +1100

    routes, controller, dto

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import {AdminRoute, VendorRoute} from './routes'
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.use('/', (req, res) => {
-    return res.json("hello from food app")
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/admin', AdminRoute)
+app.use('/vendor', VendorRoute)
 
 app.listen(8000, () => {
+    console.clear()
     console.log("app is listening on port 8000")
 })
---
+++ routes/AdminRoute.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { CreateVendor, GetVendor, GetVendorById } from '../controllers';
+
+const router = express.Router();
+
+router.post('/vendor', CreateVendor)
+router.get('/vendors', GetVendor)
+router.get('/vendor/:id', GetVendorById)
+
+
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.json({message: "from admin"})
+})
+
+
+export {router as AdminRoute};
