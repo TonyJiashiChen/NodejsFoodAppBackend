6e343750effe831469cd1f6114e77fccd1e31140
TonyJiashiChen <tonychen9661@gmail.com>
2023-10-03 19:25:05 +1100

    create vendor function

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,8 +1,11 @@
 import { Request, Response, NextFunction } from "express";
+import { CreateVendorInput } from "../dto";
 
 
 export const CreateVendor = async (req: Request, res: Response, next: NextFunction) => {
-
+    const { name, address, pincode, foodType, email, password, ownerName, phone }  = <CreateVendorInput>req.body;
+    
+    return res.json({ name, address, pincode, foodType, email, password, ownerName, phone });
 }
 
 export const GetVendor = async (req: Request, res: Response, next: NextFunction) => {
