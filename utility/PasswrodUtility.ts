8d31b645c93fa7277a6d5e891e0915872a49d0fa
TonyJiashiChen <tonychen9661@gmail.com>
2023-11-05 17:48:59 +1100

    it's been a while

--- utility/PasswrodUtility.ts
+++ utility/PasswrodUtility.ts
@@ -13,6 +13,7 @@ export const encryptPassword = async (password: string, salt: string) => {
     return await bcrypt.hash(password, salt)
 }
 
+
 export const validatePassword = async (enteredPassword: string, savedPassword: string, salt: string) => {
     return await encryptPassword(enteredPassword, salt) === savedPassword
 }
